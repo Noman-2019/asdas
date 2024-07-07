f4c328ba5bcf0a65c7c1071f1ccac1a011e5e6bb
Noman-2019 <nomandiu2019@gmail.com>
2024-07-07 13:06:42 +0600

    7-7-24

--- 30-06-24/employee class.php
+++ 30-06-24/employee class.php
@@ -14,18 +14,19 @@ class Employee {
     public function info(){
         $full="";
        $full.="Name :".$this->name."<br>";
-       $full.="age :".$this->age."<br>";
-       $full.="address :".$this->address."<br>";
+       $full.="Age :".$this->age."<br>";
+       $full.="Address :".$this->address."<br>";
        $full="salary:".$this->wage;
        return $full;
     }//end of class
-    class programmer extends Employee{
-       public $wage=10000;
-        public function bonus($percent){
-           return $this->wage*$percent;
-        }
-    }
+    
 }
+class programmer extends Employee{
+    public $wage=10000;
+     public function bonus($percent){
+        return $this->wage*$percent;
+     }
+ }
 
     $obj1 = new Employee;
     //  $obj1->name="Rahmat" ."<br>";
@@ -37,8 +38,8 @@ class Employee {
 
      echo $obj1->info();
     echo "<br>";
-    echo prog1=programmer;
-    echo "bonus:".|$prog1->bonus('.10');
+     $prog1= new programmer;
+    echo "bonus:".$prog1->bonus('.10');
 
 
 
